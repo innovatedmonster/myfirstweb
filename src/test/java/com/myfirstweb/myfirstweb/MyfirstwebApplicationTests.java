75edc2bb6d3ded08a5f28c992dcc419f72d47584
MythSam <1219341322@qq.com>
2023-11-25 15:05:21 +0800

    my firtst commit

---
+++ src/test/java/com/myfirstweb/myfirstweb/MyfirstwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirstweb.myfirstweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
