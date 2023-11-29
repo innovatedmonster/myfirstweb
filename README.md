03d4bdddfd924726febc9b875aead9ca7100ac94
innovatedmonster <1219341322@qq.com>
2023-11-29 21:57:34 +0800

    update login to git

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## 社区网页
-
+client_id = 923057d72d43fa4c456d
+token = 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
 ### 资源
 [spring官方文档](https://spring.io/guides)
 </br>[bootstrap官方文档](https://v3.bootcss.com/css/)
+</br>[github oauth文档](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/creating-an-oauth-app)
+</br>[画图文档](https://www.visual-paradigm.com)
+</br>[mvn仓库](https://mvnrepository.com/)
--- src/main/java/com/myfirstweb/myfirstweb/controller/IndexController.java
+++ src/main/java/com/myfirstweb/myfirstweb/controller/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HalloController {
+public class IndexController {
 
-    @GetMapping("/hallo")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hallo";
+        return "index";
     }
 
 }
