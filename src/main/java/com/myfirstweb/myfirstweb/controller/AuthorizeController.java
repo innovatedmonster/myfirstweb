f3f851780d25f5685439df76d5dd975beec72a3a
innovatedmonster <1219341322@qq.com>
2024-04-19 01:08:09 +0800

    add session but lost when restart the service

--- src/main/java/com/myfirstweb/myfirstweb/controller/AuthorizeController.java
+++ src/main/java/com/myfirstweb/myfirstweb/controller/AuthorizeController.java
@@ -11,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -25,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,8 +38,20 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken((accessTokenDTO));
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
-        System.out.println(user.getLogin());
-        return "index";
+
+        //获取session和cookie
+        if(user != null){
+            //登录成功
+            request.getSession().setAttribute("user", user);
+        }
+        else{
+            //登录失败
+        }
+        return "redirect:/";
+
+
+//        System.out.println(user.getId());
+//        System.out.println(user.getLogin());
+//        return "index";
     }
 }
